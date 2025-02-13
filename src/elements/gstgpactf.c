mekhti-ravnur/gst-gpac-plugin

commit 05c0c205954dbc89b605a4cd7122449d78361d17

fix: properly transition through states

src/elements/gstgpactf.c
@@ -82,14 +82,42 @@ gst_gpac_tf_pad_get_property(GObject* object,
 static void
 gst_gpac_tf_pad_init(GstGpacTransformPad* pad)
 {
+  GpacPadPrivate* priv = g_new0(GpacPadPrivate, 1);
+  priv->self = GST_PAD(pad);
+  priv->idr_period = GST_CLOCK_TIME_NONE;
+  priv->idr_last = GST_CLOCK_TIME_NONE;
+  priv->idr_next = GST_CLOCK_TIME_NONE;
+  gst_pad_set_element_private(GST_PAD(pad), priv);
+
   pad->pid = NULL;
 };
+
+static void
+gst_gpac_tf_pad_finalize(GObject* object)
+{
+  GstGpacTransformPad* pad = GST_GPAC_TF_PAD(object);
+  GpacPadPrivate* priv = gst_pad_get_element_private(GST_PAD(pad));
+
+  if (priv) {
+    if (priv->caps)
+      gst_caps_unref(priv->caps);
+    if (priv->segment)
+      gst_segment_free(priv->segment);
+    if (priv->tags)
+      gst_tag_list_unref(priv->tags);
+    g_free(priv);
+    gst_pad_set_element_private(GST_PAD(pad), NULL);
+  }
+
+  G_OBJECT_CLASS(gst_gpac_tf_pad_parent_class)->finalize(object);
+}
 
 static void
 gst_gpac_tf_pad_class_init(GstGpacTransformPadClass* klass)
 {
   GObjectClass* gobject_class = (GObjectClass*)klass;
 
+  gobject_class->finalize = GST_DEBUG_FUNCPTR(gst_gpac_tf_pad_finalize);
   gobject_class->set_property = GST_DEBUG_FUNCPTR(gst_gpac_tf_pad_set_property);
   gobject_class->get_property = GST_DEBUG_FUNCPTR(gst_gpac_tf_pad_get_property);
 
@@ -343,7 +371,7 @@ gst_gpac_tf_sink_event(GstAggregator* agg,
       gpac_memio_set_eos(GPAC_SESS_CTX(GPAC_CTX), TRUE);
       // fallthrough
     case GST_EVENT_FLUSH_START:
-      gpac_session_flush(GPAC_SESS_CTX(GPAC_CTX));
+      gpac_session_run(GPAC_SESS_CTX(GPAC_CTX), TRUE);
       gst_gpac_tf_consume(agg, GST_EVENT_TYPE(event) == GST_EVENT_EOS);
       break;
 
@@ -501,7 +529,7 @@ gst_gpac_tf_aggregate(GstAggregator* agg, gboolean timeout)
   g_queue_free(queue);
 
   // Run the filter session
-  if (gpac_session_run(GPAC_SESS_CTX(GPAC_CTX)) != GF_OK) {
+  if (gpac_session_run(GPAC_SESS_CTX(GPAC_CTX), FALSE) != GF_OK) {
     GST_ELEMENT_ERROR(
       agg, STREAM, FAILED, (NULL), ("Failed to run the GPAC session"));
     return GST_FLOW_ERROR;
@@ -570,17 +598,11 @@ gst_gpac_tf_request_new_pad(GstElement* element,
   g_free(name);
 
   // Initialize the private data
-  GpacPadPrivate* priv = g_new0(GpacPadPrivate, 1);
-  priv->self = GST_PAD(pad);
-  priv->idr_period = GST_CLOCK_TIME_NONE;
-  priv->idr_last = GST_CLOCK_TIME_NONE;
-  priv->idr_next = GST_CLOCK_TIME_NONE;
+  GpacPadPrivate* priv = gst_pad_get_element_private(GST_PAD(pad));
   if (caps) {
     priv->caps = gst_caps_copy(caps);
     priv->flags |= GPAC_PAD_CAPS_SET;
   }
-
-  gst_pad_set_element_private(GST_PAD(pad), priv);
 
   return GST_PAD(pad);
 }
@@ -608,12 +630,6 @@ gst_gpac_tf_reset(GstGpacTransform* tf)
 
         // Reset the PID
         g_object_set(GST_AGGREGATOR_PAD(pad), "pid", NULL, NULL);
-
-        // Free the private data
-        if (priv) {
-          g_free(priv);
-          gst_pad_set_element_private(pad, NULL);
-        }
         break;
       }
       case GST_ITERATOR_RESYNC:
@@ -626,10 +642,9 @@ gst_gpac_tf_reset(GstGpacTransform* tf)
   g_value_unset(&item);
   gst_iterator_free(pad_iter);
 
-  if (tf->queue) {
-    g_queue_free(tf->queue);
-    tf->queue = NULL;
-  }
+  // Empty the queue
+  if (tf->queue)
+    g_queue_clear_full(tf->queue, (GDestroyNotify)gf_filter_pck_unref);
 }
 
 static GstStateChangeReturn
@@ -732,8 +747,8 @@ gst_gpac_tf_change_state(GstElement* element, GstStateChange transition)
   switch (transition) {
     case GST_STATE_CHANGE_NULL_TO_NULL:
     case GST_STATE_CHANGE_READY_TO_NULL:
-      // Free the memory input
-      gpac_memio_free(GPAC_SESS_CTX(GPAC_CTX));
+      // Reset the element
+      gst_gpac_tf_reset(gpac_tf);
 
       // Close the session
       if (!gpac_session_close(GPAC_SESS_CTX(GPAC_CTX),
@@ -745,9 +760,6 @@ gst_gpac_tf_change_state(GstElement* element, GstStateChange transition)
 
       // Destroy the GPAC context
       gpac_destroy(GPAC_CTX);
-
-      // Reset the element
-      gst_gpac_tf_reset(gpac_tf);
       break;
 
     default:
@@ -755,6 +767,32 @@ gst_gpac_tf_change_state(GstElement* element, GstStateChange transition)
   }
 
   return ret;
+}
+
+static void
+gst_gpac_tf_finalize(GObject* object)
+{
+  GstGpacTransform* gpac_tf = GST_GPAC_TF(object);
+  GPAC_PropertyContext* ctx = GPAC_PROP_CTX(GPAC_CTX);
+
+  // Free the properties
+  while (gf_list_count(ctx->properties)) {
+    void* item = gf_list_pop_front(ctx->properties);
+    g_free(item);
+  }
+  gf_list_del(ctx->properties);
+  for (u32 i = 0; ctx->props_as_argv[i]; i++)
+    g_free(ctx->props_as_argv[i]);
+  g_free(ctx->props_as_argv);
+
+  // Free the queue
+  if (gpac_tf->queue) {
+    g_assert(g_queue_is_empty(gpac_tf->queue));
+    g_queue_free(gpac_tf->queue);
+    gpac_tf->queue = NULL;
+  }
+
+  G_OBJECT_CLASS(parent_class)->finalize(object);
 }
 
 // #MARK: Initialization
@@ -804,6 +842,9 @@ gst_gpac_tf_subclass_init(GstGpacTransformClass* klass)
   GstElementClass* gstelement_class = GST_ELEMENT_CLASS(klass);
   GstGpacTransformParams* params =
     g_type_get_qdata(G_OBJECT_CLASS_TYPE(klass), GST_GPAC_TF_PARAMS_QDATA);
+
+  // Set the finalizer
+  gobject_class->finalize = GST_DEBUG_FUNCPTR(gst_gpac_tf_finalize);
 
   // Set the property handlers
   gobject_class->set_property = GST_DEBUG_FUNCPTR(gst_gpac_tf_set_property);

include/lib/session.h
@@ -34,6 +34,9 @@ typedef struct
   GF_FilterSession* session;
   GF_Filter* memin;
   GF_Filter* memout;
+
+  /*< internal >*/
+  gboolean had_data_flow;
 } GPAC_SessionContext;
 
 /*! initializes a gpac filter session
@@ -52,20 +55,13 @@ gpac_session_init(GPAC_SessionContext* ctx, GstElement* element);
 gboolean
 gpac_session_close(GPAC_SessionContext* ctx, gboolean print_stats);
 
-/*! flushes a gpac filter session
-    \param[in] ctx the session context to flush
-    \return GF_OK if the session was flushed successfully, an error code
-   otherwise
-*/
-GF_Err
-gpac_session_flush(GPAC_SessionContext* ctx);
-
 /*! runs a gpac filter session
     \param[in] ctx the session context to run
+    \param[in] flush whether to flush the session
     \return GF_OK if the session was run successfully, an error code otherwise
 */
 GF_Err
-gpac_session_run(GPAC_SessionContext* ctx);
+gpac_session_run(GPAC_SessionContext* ctx, gboolean flush);
 
 /*! opens a gpac filter session
     \param[in] ctx the session context to open

src/lib/main.c
@@ -36,12 +36,4 @@ void
 gpac_destroy(GPAC_Context* ctx)
 {
   gf_sys_close();
-
-  // Free the properties
-  while (gf_list_count(ctx->prop.properties)) {
-    void* item = gf_list_pop_front(ctx->prop.properties);
-    g_free(item);
-  }
-  gf_list_del(ctx->prop.properties);
-  g_free(ctx->prop.props_as_argv);
 }

src/lib/properties.c
@@ -362,26 +362,46 @@ gpac_apply_properties(GPAC_PropertyContext* ctx)
   if (!ctx->properties)
     ctx->properties = gf_list_new();
 
-  // Insert a dummy "executable" name
-  gf_list_insert(ctx->properties, gf_strdup("gst"), 0);
-
   // Add the default properties
+  GF_List* override_list = gf_list_new();
   for (u32 i = 0; override_properties[i].name; i++) {
     const GF_GPACArg* arg = &override_properties[i];
     gchar* property = g_strdup_printf("--%s=%s", arg->name, arg->val);
-    gf_list_add(ctx->properties, property);
+    gf_list_add(override_list, property);
   }
 
-  ctx->props_as_argv =
-    (gchar**)g_malloc0_n(gf_list_count(ctx->properties), sizeof(gchar*));
+  // Free the previous arguments
+  if (ctx->props_as_argv) {
+    for (u32 i = 0; ctx->props_as_argv[i]; i++)
+      g_free(ctx->props_as_argv[i]);
+    g_free(ctx->props_as_argv);
+  }
+
+  // Allocate the arguments array (+1 for the executable name)
+  u32 num_properties =
+    1 + gf_list_count(ctx->properties) + gf_list_count(override_list);
+  ctx->props_as_argv = (gchar**)g_malloc0_n(num_properties, sizeof(gchar*));
+
+  // Add the executable name
+  ctx->props_as_argv[0] = g_strdup("gst");
+
   void* item;
-  for (u32 i = 0; (item = gf_list_enum(ctx->properties, &i));)
-    ctx->props_as_argv[i - 1] = (gchar*)item;
+  for (u32 i = 0; i < gf_list_count(ctx->properties); i++) {
+    item = gf_list_get(ctx->properties, i);
+    ctx->props_as_argv[i + 1] = (gchar*)g_strdup(item);
+  }
+  for (u32 i = 0; i < gf_list_count(override_list); i++) {
+    item = gf_list_get(override_list, i);
+    ctx->props_as_argv[i + 1 + gf_list_count(ctx->properties)] = (gchar*)item;
+  }
 
   // Set the gpac system arguments
-  gpac_return_val_if_fail(gf_sys_set_args((s32)gf_list_count(ctx->properties),
-                                          (const char**)ctx->props_as_argv),
-                          FALSE);
+  gpac_return_val_if_fail(
+    gf_sys_set_args((s32)num_properties, (const char**)ctx->props_as_argv),
+    FALSE);
+
+  // Free the override list
+  gf_list_del(override_list);
 
   return TRUE;
 }

src/lib/session.c
@@ -23,6 +23,7 @@
  *
  */
 #include "lib/session.h"
+#include "lib/memio.h"
 #include <gpac/list.h>
 
 #define SEP_LINK 5
@@ -102,8 +103,10 @@ gboolean
 gpac_session_close(GPAC_SessionContext* ctx, gboolean print_stats)
 {
   if (ctx->session) {
-    // Run the filter chain until the end
-    gpac_session_flush(ctx);
+    if (ctx->had_data_flow) {
+      // Run the filter chain until the end
+      gpac_session_run(ctx, TRUE);
+    }
 
     // Stop the session
     gf_fs_stop(ctx->session);
@@ -116,6 +119,8 @@ gpac_session_close(GPAC_SessionContext* ctx, gboolean print_stats)
       gf_log_set_tools_levels("all@warning", 1);
     }
 
+    gpac_memio_free(ctx);
+
     // Reset the session context
     gf_fs_del(ctx->session);
     ctx->session = NULL;
@@ -125,21 +130,7 @@ gpac_session_close(GPAC_SessionContext* ctx, gboolean print_stats)
 }
 
 GF_Err
-gpac_session_flush(GPAC_SessionContext* ctx)
-{
-  GF_Err e = GF_BAD_PARAM;
-  if (!ctx->session)
-    return e;
-
-  gf_filter_post_process_task(ctx->memin);
-  do {
-    gf_fs_run(ctx->session);
-  } while (!gf_fs_is_last_task(ctx->session));
-  return e;
-}
-
-GF_Err
-gpac_session_run(GPAC_SessionContext* ctx)
+gpac_session_run(GPAC_SessionContext* ctx, gboolean flush)
 {
   if (!ctx->session)
     return GF_BAD_PARAM;
@@ -149,7 +140,8 @@ gpac_session_run(GPAC_SessionContext* ctx)
   guint32 steps = 100;
   do {
     e = gf_fs_run(ctx->session);
-  } while (!gf_fs_is_last_task(ctx->session) && e == GF_OK && steps--);
+  } while (!gf_fs_is_last_task(ctx->session) &&
+           (flush || (e == GF_OK && steps--)));
 
   // Check errors
   if ((e = gf_fs_get_last_connect_error(ctx->session)) != GF_OK) {
@@ -168,6 +160,11 @@ gpac_session_run(GPAC_SessionContext* ctx)
                       (NULL));
     return e;
   }
+
+  // Mark that we had data flow
+  if (!flush)
+    ctx->had_data_flow = TRUE;
+
   return GF_OK;
 }
 

tests/src/state_transition.cc
@@ -1,0 +1,37 @@
+#include "common.hpp"
+
+TEST_F(GstTestFixture, StatePausedToNullToPlaying)
+{
+  this->SetUpPipeline({ false, "x264enc", 5 });
+  GstElement* muxer =
+    gst_element_factory_make_full("gpacmp4mx", "cdur", 5.0, NULL);
+  GstElement* fake_sink = gst_element_factory_make("fakesink", NULL);
+  gst_bin_add_many(GST_BIN(pipeline), muxer, fake_sink, NULL);
+
+  if (!gst_element_link(this->GetLastElement(), muxer) ||
+      !gst_element_link(muxer, fake_sink)) {
+    g_error("Failed to link elements");
+    return;
+  }
+
+  // Start the pipeline
+  GstStateChangeReturn ret = gst_element_set_state(pipeline, GST_STATE_PAUSED);
+  if (ret == GST_STATE_CHANGE_FAILURE) {
+    g_error("Failed to start pipeline");
+  }
+
+  // Set the NULL state
+  ret = gst_element_set_state(pipeline, GST_STATE_NULL);
+  if (ret == GST_STATE_CHANGE_FAILURE) {
+    g_error("Failed to set pipeline to NULL state");
+  }
+
+  // Back to the PLAYING state
+  ret = gst_element_set_state(pipeline, GST_STATE_PLAYING);
+  if (ret == GST_STATE_CHANGE_FAILURE) {
+    g_error("Failed to start pipeline");
+  }
+
+  // Wait for the EOS
+  this->WaitForEOS();
+}

commit 2ada29d9b5de223657c52918d88ddfcfa54e4755

fix: regression from 05c0c20

src/elements/gstgpactf.c
@@ -781,9 +781,11 @@ gst_gpac_tf_finalize(GObject* object)
     g_free(item);
   }
   gf_list_del(ctx->properties);
-  for (u32 i = 0; ctx->props_as_argv[i]; i++)
-    g_free(ctx->props_as_argv[i]);
-  g_free(ctx->props_as_argv);
+  if (ctx->props_as_argv) {
+    for (u32 i = 0; ctx->props_as_argv[i]; i++)
+      g_free(ctx->props_as_argv[i]);
+    g_free(ctx->props_as_argv);
+  }
 
   // Free the queue
   if (gpac_tf->queue) {