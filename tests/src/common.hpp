308361850e23d1d440daf6964b1cae2ab90461e0
DenizUgur <DenizUgur@users.noreply.github.com>
2025-02-23 13:50:38 -0800

    test: add a test for handling multiple sink pads

--- tests/src/common.hpp
+++ tests/src/common.hpp
@@ -3,6 +3,7 @@
 #include <gst/gst.h>
 #include <gtest/gtest.h>
 #include <thread>
+#include <vector>
 
 #define ROUND_UP(val, round) (((val) + (round) - 1) / (round) * (round))
 
@@ -11,6 +12,8 @@ struct PipelineConfiguration
   bool use_tee = false;
   std::string encoder = "x264enc";
   int num_buffers = 300;
+  std::string source = "videotestsrc";
+  std::string caps = "video/x-raw, framerate=30/1";
 };
 
 class GstAppSink
@@ -69,9 +72,9 @@ public:
 class GstTestFixture : public ::testing::Test
 {
 private:
-  GstElement* source;
-  GstElement* encoder;
-  GstElement* last_element;
+  std::vector<GstElement*> sources;
+  std::vector<GstElement*> source_sinks;
+  std::vector<GstElement*> encoders;
   bool is_eos = false;
 
 protected:
@@ -80,23 +83,36 @@ protected:
 
   static void SetUpTestSuite() { gst_init(NULL, NULL); }
   static void TearDownTestSuite() { gst_deinit(); }
-  GstElement* GetLastElement() { return last_element; }
-  GstElement* GetEncoder() { return encoder; }
-  void SetLive(bool is_live) { g_object_set(source, "is-live", is_live, NULL); }
+  GstElement* GetSource(int i = 0) { return sources[i]; }
+  GstElement* GetLastElement(int i = 0) { return source_sinks[i]; }
+  GstElement* GetEncoder(int i = 0) { return encoders[i]; }
+  void SetLive(bool is_live)
+  {
+    for (GstElement* source : sources)
+      g_object_set(source, "is-live", is_live, NULL);
+  }
 
   void SetUp() override
   {
-    source = gst_element_factory_make_full(
-      "videotestsrc", "do-timestamp", TRUE, "is-live", FALSE, NULL);
-    GstElement* capsfilter = gst_element_factory_make_full(
-      "capsfilter",
-      "caps",
-      gst_caps_from_string("video/x-raw, framerate=30/1"),
-      NULL);
-
     // Create the pipeline
     pipeline = gst_pipeline_new("test-pipeline");
-    if (!pipeline || !source || !capsfilter) {
+    if (!pipeline) {
+      g_error("Failed to create elements");
+      return;
+    }
+  }
+
+  void SetUpPipeline(PipelineConfiguration cfg)
+  {
+    GstElement* source = gst_element_factory_make_full(
+      cfg.source.c_str(), "do-timestamp", TRUE, "is-live", FALSE, NULL);
+    sources.push_back(source);
+
+    GstElement* capsfilter = gst_element_factory_make_full(
+      "capsfilter", "caps", gst_caps_from_string(cfg.caps.c_str()), NULL);
+
+    // Check if the elements were created
+    if (!source || !capsfilter) {
       g_error("Failed to create elements");
       return;
     }
@@ -109,33 +125,34 @@ protected:
       g_error("Failed to link elements");
       return;
     }
-    last_element = capsfilter;
-  }
 
-  void SetUpPipeline(PipelineConfiguration cfg)
-  {
     // Reconfigure the source
     g_object_set(source, "num-buffers", cfg.num_buffers, NULL);
 
     // Create the encoder
-    encoder = gst_element_factory_make_full(cfg.encoder.c_str(), NULL);
+    GstElement* encoder =
+      gst_element_factory_make_full(cfg.encoder.c_str(), NULL);
     if (!encoder) {
       g_error("Failed to create elements");
       return;
     }
+    encoders.push_back(encoder);
 
     // Add the encoder to the pipeline
     gst_bin_add(GST_BIN(pipeline), encoder);
 
     // Link the encoder to the last element
-    if (!gst_element_link(last_element, encoder)) {
+    if (!gst_element_link(capsfilter, encoder)) {
       g_error("Failed to link elements");
       return;
     }
-    last_element = encoder;
+    source_sinks.push_back(encoder);
 
     // Add the tee if needed
     if (cfg.use_tee) {
+      // Cannot handle multiple sources
+      assert(source_sinks.size() == 1);
+
       tee = gst_element_factory_make("tee", NULL);
       if (!tee) {
         g_error("Failed to create elements");
@@ -145,11 +162,11 @@ protected:
       gst_bin_add(GST_BIN(pipeline), tee);
 
       // Link the elements
-      if (!gst_element_link(last_element, tee)) {
+      if (!gst_element_link(source_sinks[0], tee)) {
         g_error("Failed to link elements");
         return;
       }
-      last_element = tee;
+      source_sinks[0] = tee;
     }
   }
 
--- tests/src/misc.cc
+++ tests/src/misc.cc
@@ -44,3 +44,53 @@ TEST_F(GstTestFixture, NonFragmented)
   gf_sys_close();
   fs::remove(file);
 }
+
+TEST_F(GstTestFixture, MultipleInputs)
+{
+  this->SetUpPipeline({ false, "x264enc", 30 });
+  this->SetUpPipeline(
+    { false, "avenc_aac", 1, "audiotestsrc", "audio/x-raw, rate=44100" });
+
+  // Set samples per buffer for the audio source
+  GstElement* audio_source = this->GetSource(1);
+  g_object_set(audio_source, "samplesperbuffer", 44100, NULL);
+
+  // Create the muxer
+  GstElement* gpaccmafmux = gst_element_factory_make("gpaccmafmux", NULL);
+
+  // Set the destination options
+  std::string file = fs::temp_directory_path().string() + "/" + "multi.mp4";
+  GstElement* sink =
+    gst_element_factory_make_full("filesink", "location", file.c_str(), NULL);
+
+  // Add the elements to the pipeline
+  gst_bin_add_many(GST_BIN(pipeline), gpaccmafmux, sink, NULL);
+
+  // Link the elements
+  if (!gst_element_link(this->GetLastElement(0), gpaccmafmux) ||
+      !gst_element_link(this->GetLastElement(1), gpaccmafmux) ||
+      !gst_element_link(gpaccmafmux, sink)) {
+    g_error("Failed to link elements");
+    return;
+  }
+
+  this->StartPipeline();
+  this->WaitForEOS();
+
+  // Read the file
+  ASSERT_TRUE(fs::exists(file));
+  gf_sys_init(GF_MemTrackerNone, NULL);
+  GF_ISOFile* isom = gf_isom_open(file.c_str(), GF_ISOM_OPEN_READ, NULL);
+  ASSERT_TRUE(isom != NULL);
+
+  // Check the track and sample count
+  EXPECT_EQ(gf_isom_get_track_count(isom), 2);
+  EXPECT_EQ(gf_isom_get_sample_count(isom, 1), 30);
+  EXPECT_EQ(gf_isom_get_sample_count(isom, 2), 45);
+  EXPECT_TRUE(gf_isom_is_fragmented(isom));
+
+  // Close the file
+  gf_isom_close(isom);
+  gf_sys_close();
+  fs::remove(file);
+}
