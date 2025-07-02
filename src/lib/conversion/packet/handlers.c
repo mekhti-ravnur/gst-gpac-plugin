mekhti-ravnur/gst-gpac-plugin

commit fff2606372cda8a7da6d9ac22ab1ef85f7ad9b77

feat: add id3 handling

src/lib/conversion/packet/handlers.c
@@ -1,0 +1,100 @@
+/*
+ *			GPAC - Multimedia Framework C SDK
+ *
+ *			Authors: Deniz Ugur, Romain Bouqueau, Sohaib Larbi
+ *			Copyright (c) Motion Spell
+ *				All rights reserved
+ *
+ *  This file is part of the GPAC/GStreamer wrapper
+ *
+ *  This GPAC/GStreamer wrapper is free software; you can redistribute it
+ *  and/or modify it under the terms of the GNU Affero General Public License
+ *  as published by the Free Software Foundation; either version 3, or (at
+ *  your option) any later version.
+ *
+ *  This GPAC/GStreamer wrapper is distributed in the hope that it will be
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public
+ *  License along with this library; see the file LICENSE.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#include "lib/packet.h"
+
+#include <gpac/internal/id3.h>
+#include <gst/gst.h>
+
+gboolean
+id3_handler(GPAC_PCK_PROP_IMPL_ARGS)
+{
+  const GstMetaInfo* info = gst_meta_get_info("Id3Meta");
+  if (!info)
+    return FALSE;
+
+  GstMeta* meta = gst_buffer_get_meta(buffer, info->api);
+  if (!meta)
+    return FALSE;
+
+  GstStructure* s = ((GstCustomMeta*)meta)->structure;
+  const GValue* tags_val = gst_structure_get_value(s, "tags");
+
+  if (!tags_val || !GST_VALUE_HOLDS_LIST(tags_val))
+    return FALSE;
+
+  guint n = gst_value_list_get_size(tags_val);
+  if (n == 0)
+    return FALSE;
+
+  // Create new list to store the tags
+  GF_List* tag_list = gf_list_new();
+  guint64 pts = gf_filter_pck_get_cts(pck);
+  guint32 timescale = gf_filter_pck_get_timescale(pck);
+
+  for (guint i = 0; i < n; ++i) {
+    const GValue* tag_val = gst_value_list_get_value(tags_val, i);
+    const GstStructure* tag_struct = g_value_get_boxed(tag_val);
+
+    const GValue* data_val = gst_structure_get_value(tag_struct, "data");
+    if (data_val && G_VALUE_HOLDS(data_val, GST_TYPE_BUFFER)) {
+      GstBuffer* tag_buf = g_value_get_boxed(data_val);
+      g_auto(GstBufferMapInfo) map = GST_MAP_INFO_INIT;
+      if (gst_buffer_map(tag_buf, &map, GST_MAP_READ)) {
+        // Create a new ID3 tag
+        GF_ID3_TAG* tag;
+        GF_SAFEALLOC(tag, GF_ID3_TAG);
+        gf_id3_tag_new(tag, timescale, pts, map.data, map.size);
+
+        // Add the tag to the list
+        gf_list_add(tag_list, tag);
+      }
+    }
+  }
+
+  // Write the tags to a bitstream
+  GF_BitStream* bs = gf_bs_new(NULL, 0, GF_BITSTREAM_WRITE);
+  gf_id3_list_to_bitstream(tag_list, bs);
+
+  // Free the tag list
+  GF_ID3_TAG* tag;
+  while ((tag = (GF_ID3_TAG*)gf_list_pop_back(tag_list)))
+    gf_id3_tag_free(tag);
+  gf_list_del(tag_list);
+
+  // Export the bitrstream
+  u8* data;
+  u32 size;
+  gf_bs_get_content(bs, &data, &size);
+
+  // Set the ID3 tags as a property on the packet
+  gf_filter_pck_set_property_str(pck, "id3", &PROP_DATA(data, size));
+
+  // Free the data
+  gf_free(data);
+  gf_bs_del(bs);
+
+  return TRUE;
+}

src/lib/conversion/packet/registry.h
@@ -25,24 +25,43 @@
 
 #pragma once
 
-#include <gpac/filters.h>
-#include <gst/gst.h>
+#include "lib/packet.h"
 
-#include "lib/pid.h"
-#include "lib/time.h"
+//
+// Macros for declaring packet property handlers
+//
 
-#define GPAC_PCK_PROP_IMPL_ARGS_NO_ELEMENT   \
-  GpacPadPrivate *priv, GF_FilterPacket *pck
-#define GPAC_PCK_PROP_IMPL_ARGS                         \
-  GstBuffer *buffer, GPAC_PCK_PROP_IMPL_ARGS_NO_ELEMENT
+#define GPAC_PROP_IMPL_DECL(prop_nickname)                   \
+  gboolean prop_nickname##_handler(GPAC_PCK_PROP_IMPL_ARGS);
 
-/*! creates a new packet from the given buffer
-    \param[in] buffer the buffer to create the packet from
-    \param[in] priv the private data of the pad
-    \param[in] pid the pid to create the packet for
-    \return the new packet
-*/
-GF_FilterPacket*
-gpac_pck_new_from_buffer(GstBuffer* buffer,
-                         GpacPadPrivate* priv,
-                         GF_FilterPid* pid);
+//
+// Macros for declaring property handlers
+//
+
+#define GPAC_PROP_DEFINE(prop_4cc, prop_nickname) \
+  { prop_4cc, NULL, prop_nickname##_handler }
+
+#define GPAC_PROP_DEFINE_STR(prop_str, prop_nickname) \
+  { 0, prop_str, prop_nickname##_handler }
+
+//
+// Property handler declarations
+//
+GPAC_PROP_IMPL_DECL(id3);
+
+typedef struct
+{
+  u32 prop_4cc;
+  const gchar* prop_str;
+
+  gboolean (*handler)(GPAC_PCK_PROP_IMPL_ARGS);
+} prop_registry_entry;
+
+static prop_registry_entry prop_registry[] = { GPAC_PROP_DEFINE_STR("id3",
+                                                                    id3) };
+
+u32
+gpac_pck_get_num_supported_props()
+{
+  return G_N_ELEMENTS(prop_registry);
+}

src/lib/packet.c
@@ -24,6 +24,8 @@
  */
 
 #include "lib/packet.h"
+#include "conversion/packet/registry.h"
+#include "utils.h"
 
 static void
 gpac_pck_destructor(GF_Filter* filter, GF_FilterPid* PID, GF_FilterPacket* pck)
@@ -87,6 +89,60 @@ fail:
                      GST_TIME_ARGS(time)),
                     (NULL));
   return 0;
+}
+
+void
+gpac_configure_video(GstBuffer* buffer,
+                     GpacPadPrivate* priv,
+                     GF_FilterPacket* packet)
+{
+  // Decide on sample dependency flags
+  gboolean is_delta =
+    GST_BUFFER_FLAG_IS_SET(buffer, GST_BUFFER_FLAG_DELTA_UNIT);
+  gboolean is_droppable =
+    GST_BUFFER_FLAG_IS_SET(buffer, GST_BUFFER_FLAG_DROPPABLE);
+  gboolean is_discontinuity =
+    GST_BUFFER_FLAG_IS_SET(buffer, GST_BUFFER_FLAG_DISCONT);
+
+  // Set the SAP type for video streams
+  gf_filter_pck_set_sap(packet,
+                        is_delta ? GF_FILTER_SAP_NONE : GF_FILTER_SAP_1);
+
+  // Note that acceptance tests doesn't like SAP, is_leading, sample_depends_on
+  u32 flags = 0;
+
+  // is_leading
+  if (is_delta)
+    flags |= (priv->last_frame_was_keyframe ? 3 : 2) << 6;
+
+  // sample_depends_on
+  if (is_discontinuity)
+    flags |= 2 << 4;
+  else
+    flags |= ((is_delta ? 1 : 2) << 4);
+
+  flags |= ((is_droppable ? 2 : 1) << 2); // sample_is_depended_on
+  flags |= (2 << 0);                      // sample_has_redundancy
+
+  gf_filter_pck_set_dependency_flags(packet, flags);
+  priv->last_frame_was_keyframe = !is_delta;
+}
+
+void
+gpac_pck_prop_configure(GPAC_PCK_PROP_IMPL_ARGS)
+{
+  // Check arguments
+  g_return_if_fail(buffer != NULL);
+  g_return_if_fail(priv != NULL);
+  g_return_if_fail(pck != NULL);
+
+  // Go through the property registry
+  for (u32 i = 0; i < gpac_pck_get_num_supported_props(); i++) {
+    prop_registry_entry* entry = &prop_registry[i];
+
+    // Run the handler for the property
+    entry->handler(buffer, priv, pck);
+  }
 }
 
 GF_FilterPacket*
@@ -169,41 +225,13 @@ gpac_pck_new_from_buffer(GstBuffer* buffer,
     gf_filter_pid_get_property(pid, GF_PROP_PID_STREAM_TYPE);
   gboolean is_video = p_typ && p_typ->value.uint == GF_STREAM_VISUAL;
 
-  // For non-video streams, we're done
-  if (!is_video)
-    goto finish;
+  // For video streams, we need further configuration
+  if (is_video) {
+    gpac_configure_video(buffer, priv, packet);
+  }
 
-  // Decide on sample dependency flags
-  gboolean is_delta =
-    GST_BUFFER_FLAG_IS_SET(buffer, GST_BUFFER_FLAG_DELTA_UNIT);
-  gboolean is_droppable =
-    GST_BUFFER_FLAG_IS_SET(buffer, GST_BUFFER_FLAG_DROPPABLE);
-  gboolean is_discontinuity =
-    GST_BUFFER_FLAG_IS_SET(buffer, GST_BUFFER_FLAG_DISCONT);
+  // Configure the packet properties
+  gpac_pck_prop_configure(buffer, priv, packet);
 
-  // Set the SAP type for video streams
-  gf_filter_pck_set_sap(packet,
-                        is_delta ? GF_FILTER_SAP_NONE : GF_FILTER_SAP_1);
-
-  // Note that acceptance tests doesn't like SAP, is_leading, sample_depends_on
-  u32 flags = 0;
-
-  // is_leading
-  if (is_delta)
-    flags |= (priv->last_frame_was_keyframe ? 3 : 2) << 6;
-
-  // sample_depends_on
-  if (is_discontinuity)
-    flags |= 2 << 4;
-  else
-    flags |= ((is_delta ? 1 : 2) << 4);
-
-  flags |= ((is_droppable ? 2 : 1) << 2); // sample_is_depended_on
-  flags |= (2 << 0);                      // sample_has_redundancy
-
-  gf_filter_pck_set_dependency_flags(packet, flags);
-  priv->last_frame_was_keyframe = !is_delta;
-
-finish:
   return packet;
 }

commit f3bcab707625690662b7d02eb13d38f247bc2df6

fix: regression from fff2606372cda8a7da6d9ac22ab1ef85f7ad9b77

src/lib/conversion/packet/handlers.c
@@ -25,7 +25,7 @@
 
 #include "lib/packet.h"
 
-#include <gpac/internal/id3.h>
+#include <gpac/id3.h>
 #include <gst/gst.h>
 
 gboolean